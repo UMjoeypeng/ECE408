32a32a01f1a77eafdf65d561e200defd603ae982
Joey <peng30@illinois.edu>
2022-12-04 02:48:23 -0600

    start m3

--- Project/custom/new-forward.cu
+++ Project/custom/new-forward.cu
@@ -1,17 +1,20 @@
 #include <cmath>
 #include <iostream>
-#include "gpu-new-forward.h"
 
+#include "gpu-new-forward.h"
 
 #define TILE_WIDTH 16
 // #define TILE_SIZE 256
 
-__global__ void conv_forward_kernel(float *output, const float *input, const float *mask, const int Batch, const int Map_out, const int Channel, const int Height, const int Width, const int K)
-{
+__global__ void conv_forward_kernel(float *output, const float *input,
+                                    const float *mask, const int Batch,
+                                    const int Map_out, const int Channel,
+                                    const int Height, const int Width,
+                                    const int K, const int tile_size) {
     /*
     Modify this function to implement the forward pass described in Chapter 16.
-    We have added an additional dimension to the tensors to support an entire mini-batch
-    The goal here is to be correct AND fast.
+    We have added an additional dimension to the tensors to support an entire
+    mini-batch The goal here is to be correct AND fast.
 
     Function paramter definitions:
     output - output
@@ -27,83 +30,95 @@ __global__ void conv_forward_kernel(float *output, const float *input, const flo
 
     const int Height_out = Height - K + 1;
     const int Width_out = Width - K + 1;
-    // (void)Height_out; // silence declared but never referenced warning. remove this line when you start working
-    // (void)Width_out; // silence declared but never referenced warning. remove this line when you start working
+    // (void)Height_out; // silence declared but never referenced warning.
+    // remove this line when you start working (void)Width_out; // silence
+    // declared but never referenced warning. remove this line when you start
+    // working
 
-    // We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
-    // An example use of these macros:
-    // float a = in_4d(0,0,0,0)
-    // out_4d(0,0,0,0) = a
+    // We have some nice #defs for you below to simplify indexing. Feel free to
+    // use them, or create your own. An example use of these macros: float a =
+    // in_4d(0,0,0,0) out_4d(0,0,0,0) = a
 
-    #define out_4d(i3, i2, i1, i0) output[(i3) * (Map_out * Height_out * Width_out) + (i2) * (Height_out * Width_out) + (i1) * (Width_out) + i0]
-    #define in_4d(i3, i2, i1, i0) input[(i3) * (Channel * Height * Width) + (i2) * (Height * Width) + (i1) * (Width) + i0]
-    #define mask_4d(i3, i2, i1, i0) mask[(i3) * (Channel * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+#define out_4d(i3, i2, i1, i0)                         \
+    output[(i3) * (Map_out * Height_out * Width_out) + \
+           (i2) * (Height_out * Width_out) + (i1) * (Width_out) + i0]
+#define in_4d(i3, i2, i1, i0)                                           \
+    input[(i3) * (Channel * Height * Width) + (i2) * (Height * Width) + \
+          (i1) * (Width) + i0]
+#define mask_4d(i3, i2, i1, i0) \
+    mask[(i3) * (Channel * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
     // Insert your GPU convolution kernel code here
     int W_out = Width - K + 1;
     int H_out = Height - K + 1;
-    int W_grid = ceil(W_out / (float) TILE_WIDTH);
-    int H_grid = ceil(H_out / (float) TILE_WIDTH);
-    // int Z = H_grid * W_grid;
+    
+    __shared__ float input_tile[tile_size][tile_size][tile_size];
+    __shared__ float mask_tile[tile_size][tile_size][tile_size];
 
-    int n, m, h0, w0, h_base, w_base, h, w;
-    int X_tile_width = TILE_WIDTH + K - 1;
-    extern __shared__ float shmem[];
-    float* X_shared = &shmem[0];
-    float* W_shared = &shmem[X_tile_width * X_tile_width];
-    n = blockIdx.x;
-    m = blockIdx.y;
-    h0 = threadIdx.x;
-    w0 = threadIdx.y;
-    h_base = (blockIdx.z / W_grid) * TILE_WIDTH;
-    w_base = (blockIdx.z % W_grid) * TILE_WIDTH;
-    h = h_base + h0;
-    w = w_base + w0;
+    int unroll_mask_width = Channel * K * K;
+    int unroll_mask_height = Map_out;
+    int unroll_input_width = H_out * W_out;
+    int unroll_input_height = Channel * K * K;
 
-    float acc = 0;
-    int c, i, j, p, q;
-    for (c = 0; c < Channel; c++) {
-        if((h0 < K) && (w0 < K)){
-            W_shared[h0 * K + w0] = mask_4d(m, c, h0, w0);
-        }
-        __syncthreads();
-        // printf("wqq\n");
-        for(i = h; i < h_base + X_tile_width; i += TILE_WIDTH) {
-            for(j = w; j < w_base + X_tile_width; j += TILE_WIDTH){
-                // printf("%d %d\n", i, j);
-                if(i < Height && j < Width) {
-                    X_shared[(i - h_base) * X_tile_width + j - w_base] = in_4d(n, c, i, j);
-                }
-                else {
-                    X_shared[(i - h_base) * X_tile_width + j - w_base] = 0;
-                }
+    int tx = threadIdx.x, ty = threadIdx.y, tz = threadIdx.z;
+
+    int x = blockIdx.x * tile_size + x; // width & height
+    int y = blockIdx.y * tile_size + y; // Output channel
+    int z = blockIdx.z * tile_size + z; // Batch
+
+    int h_out = x / W_out;  // Which pixel in the output image
+    int w_out = x % W_out;
+
+                            
+    float ans = 0.0;
+    
+
+    if(z < Batch){
+        for(int i = 0; i < ceil((float)unroll_mask_width / (float)tile_size); i++){
+            int tile_x = i * tile_size + tx;
+            int tile_y = i * tile_size + ty;
+            int p = (tile_x / (K*K)) / K;
+            int q = (tile_x / (K*K)) % K;
+
+            if(y < unroll_mask_height && tile_y < unroll_mask_height){
+                mask_tile[tz][ty][tx] = mask[y*Channel * K * K+tile_x];
+            } else {
+                mask_tile[tz][ty][tx] = 0;
             }
-        }
 
-        __syncthreads();
-        // printf("qwq\n");
-        for(p = 0; p < K; p++){
-            for(q = 0; q < K; q++){
-                acc += X_shared[(h0 + p) * X_tile_width + w0 + q] * W_shared[p * K + q];
+            if(tile_y < unroll_input_height && x < unroll_input_width){
+                input_tile[tz][tx][ty] = in_4d(z, tile_x / (K * K), h_out + p, w_out + q);
+            } else {
+                input_tile[tz][tx][ty] = 0;
             }
+
+            __syncthreads();
+
+            for(int j = 0; j< tile_size;j++){
+                ans += mask_tile[tz][ty][k] * input_mask[tz][tx][k];
+            }
+            __syncthreads();
         }
-        __syncthreads();
-        // printf("qqq\n");
+        if(x < unroll_input_width && y < unroll_mask_height){
+            output[z * unroll_input_width * unroll_mask_height + y * unroll_input_width + x] = ans;
+        }
     }
-    if(n < Batch && m < Map_out && h < Height_out && w < Width_out){
-        out_4d(n, m, h, w) = acc;
-    }
-    #undef out_4d
-    #undef in_4d
-    #undef mask_4d
+                 
+
+#undef out_4d
+#undef in_4d
+#undef mask_4d
 }
 
-	
-__host__ void GPUInterface::conv_forward_gpu_prolog(const float *host_output, const float *host_input, const float *host_mask, float **device_output_ptr, float **device_input_ptr, float **device_mask_ptr, const int Batch, const int Map_out, const int Channel, const int Height, const int Width, const int K)
-{
+__host__ void GPUInterface::conv_forward_gpu_prolog(
+    const float *host_output, const float *host_input, const float *host_mask,
+    float **device_output_ptr, float **device_input_ptr,
+    float **device_mask_ptr, const int Batch, const int Map_out,
+    const int Channel, const int Height, const int Width, const int K) {
     // Allocate memory and copy over the relevant data structures to the GPU
 
-    // We pass double pointers for you to initialize the relevant device pointers,
+    // We pass double pointers for you to initialize the relevant device
+    // pointers,
     //  which are passed to the other two functions.
 
     const int Height_out = Height - K + 1;
@@ -115,8 +130,9 @@ __host__ void GPUInterface::conv_forward_gpu_prolog(const float *host_output, co
     cudaMalloc((void **)device_output_ptr, size_output);
     cudaMalloc((void **)device_mask_ptr, size_mask);
 
-    cudaMemcpy(*device_input_ptr, host_input, size_input,cudaMemcpyHostToDevice);
-    cudaMemcpy(*device_mask_ptr, host_mask, size_mask,cudaMemcpyHostToDevice);
+    cudaMemcpy(*device_input_ptr, host_input, size_input,
+               cudaMemcpyHostToDevice);
+    cudaMemcpy(*device_mask_ptr, host_mask, size_mask, cudaMemcpyHostToDevice);
     // Useful snippet for error checking
     // cudaError_t error = cudaGetLastError();
     // if(error != cudaSuccess)
@@ -124,57 +140,88 @@ __host__ void GPUInterface::conv_forward_gpu_prolog(const float *host_output, co
     //     std::cout<<"CUDA error: "<<cudaGetErrorString(error)<<std::endl;
     //     exit(-1);
     // }
-
 }
 
-
-__host__ void GPUInterface::conv_forward_gpu(float *device_output, const float *device_input, const float *device_mask, const int Batch, const int Map_out, const int Channel, const int Height, const int Width, const int K)
-{
+__host__ void GPUInterface::conv_forward_gpu(
+    float *device_output, const float *device_input, const float *device_mask,
+    const int Batch, const int Map_out, const int Channel, const int Height,
+    const int Width, const int K) {
     // Set the kernel dimensions and call the kernel
     int W_out = Width - K + 1;
     int H_out = Height - K + 1;
-    int W_grid = ceil(W_out / (float) TILE_WIDTH);
-    int H_grid = ceil(H_out / (float) TILE_WIDTH);
-    int Z = H_grid * W_grid;
-    dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
-    dim3 gridDim(Batch, Map_out, Z);
-    size_t shmem_size = sizeof(float) * ((TILE_WIDTH + K - 1) * (TILE_WIDTH + K - 1) + K * K);
-    conv_forward_kernel<<<gridDim, blockDim, shmem_size>>>(device_output, device_input, device_mask, Batch, Map_out, Channel, Height, Width, K);
+
+    int W_unroll = Channel * K * K;
+    int H_unroll = H_out * W_out;
+    float *X_unrolled;
+    cudaMalloc((void **)X_unrolled, H_unroll * W_unroll * sizeof(float));
+
+    int num_threads_unroll = Channel * H_out * W_out;
+    int num_blocks_unroll = ceil(num_threads_unroll / 1024);
+    unroll_kernel<<<num_blocks_unroll, 1024>>>(Batch, Channel, Height, Width, K,
+                                               *device_input, *X_unrolled);
+    cudaDeviceSynchronize();
+
+    
+
+
+    
+
+
+    // int W_grid = ceil(W_out / (float)TILE_WIDTH);
+    // int H_grid = ceil(H_out / (float)TILE_WIDTH);
+    // int Z = H_grid * W_grid;
+    // dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
+    // dim3 gridDim(Batch, Map_out, Z);
+    // size_t shmem_size =
+    //     sizeof(float) * ((TILE_WIDTH + K - 1) * (TILE_WIDTH + K - 1) + K * K);
+    // conv_forward_kernel<<<gridDim, blockDim, shmem_size>>>(
+    //     device_output, device_input, device_mask, Batch, Map_out, Channel,
+    //     Height, Width, K);
     cudaDeviceSynchronize();
 }
 
-
-__host__ void GPUInterface::conv_forward_gpu_epilog(float *host_output, float *device_output, float *device_input, float *device_mask, const int Batch, const int Map_out, const int Channel, const int Height, const int Width, const int K)
-{
+__host__ void GPUInterface::conv_forward_gpu_epilog(
+    float *host_output, float *device_output, float *device_input,
+    float *device_mask, const int Batch, const int Map_out, const int Channel,
+    const int Height, const int Width, const int K) {
     // Copy the output back to host
     const int Height_out = Height - K + 1;
     const int Width_out = Width - K + 1;
-    cudaMemcpy(host_output, device_output, (Batch * Map_out * Height_out * Width_out) * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(host_output, device_output,
+               (Batch * Map_out * Height_out * Width_out) * sizeof(float),
+               cudaMemcpyDeviceToHost);
     // Free device memory
     cudaFree(device_output);
     cudaFree(device_input);
     cudaFree(device_mask);
 }
 
-
-__host__ void GPUInterface::get_device_properties()
-{
+__host__ void GPUInterface::get_device_properties() {
     int deviceCount;
     cudaGetDeviceCount(&deviceCount);
 
-    for(int dev = 0; dev < deviceCount; dev++)
-    {
+    for (int dev = 0; dev < deviceCount; dev++) {
         cudaDeviceProp deviceProp;
         cudaGetDeviceProperties(&deviceProp, dev);
 
-        std::cout<<"Device "<<dev<<" name: "<<deviceProp.name<<std::endl;
-        std::cout<<"Computational capabilities: "<<deviceProp.major<<"."<<deviceProp.minor<<std::endl;
-        std::cout<<"Max Global memory size: "<<deviceProp.totalGlobalMem<<std::endl;
-        std::cout<<"Max Constant memory size: "<<deviceProp.totalConstMem<<std::endl;
-        std::cout<<"Max Shared memory size per block: "<<deviceProp.sharedMemPerBlock<<std::endl;
-        std::cout<<"Max threads per block: "<<deviceProp.maxThreadsPerBlock<<std::endl;
-        std::cout<<"Max block dimensions: "<<deviceProp.maxThreadsDim[0]<<" x, "<<deviceProp.maxThreadsDim[1]<<" y, "<<deviceProp.maxThreadsDim[2]<<" z"<<std::endl;
-        std::cout<<"Max grid dimensions: "<<deviceProp.maxGridSize[0]<<" x, "<<deviceProp.maxGridSize[1]<<" y, "<<deviceProp.maxGridSize[2]<<" z"<<std::endl;
-        std::cout<<"Warp Size: "<<deviceProp.warpSize<<std::endl;
+        std::cout << "Device " << dev << " name: " << deviceProp.name
+                  << std::endl;
+        std::cout << "Computational capabilities: " << deviceProp.major << "."
+                  << deviceProp.minor << std::endl;
+        std::cout << "Max Global memory size: " << deviceProp.totalGlobalMem
+                  << std::endl;
+        std::cout << "Max Constant memory size: " << deviceProp.totalConstMem
+                  << std::endl;
+        std::cout << "Max Shared memory size per block: "
+                  << deviceProp.sharedMemPerBlock << std::endl;
+        std::cout << "Max threads per block: " << deviceProp.maxThreadsPerBlock
+                  << std::endl;
+        std::cout << "Max block dimensions: " << deviceProp.maxThreadsDim[0]
+                  << " x, " << deviceProp.maxThreadsDim[1] << " y, "
+                  << deviceProp.maxThreadsDim[2] << " z" << std::endl;
+        std::cout << "Max grid dimensions: " << deviceProp.maxGridSize[0]
+                  << " x, " << deviceProp.maxGridSize[1] << " y, "
+                  << deviceProp.maxGridSize[2] << " z" << std::endl;
+        std::cout << "Warp Size: " << deviceProp.warpSize << std::endl;
     }
 }
